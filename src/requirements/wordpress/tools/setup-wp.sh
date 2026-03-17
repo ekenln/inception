14cd397b8a58dfc69b7b783385ecb60160381f41
ekenln <eklund.ellen.ee@gmail.com>
2026-03-17 20:04:18 +0100

    wp is broken

---
+++ src/requirements/wordpress/tools/setup-wp.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+mkdir -p /var/www/wordpress
+cd /var/www/wordpress
+
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+if [ ! -f wp-login.php ]; then
+    wp core download --allow-root
+fi
+
+sleep 10
+
+if [ ! -f wp-config.php ]; then
+	wp config create --allow-root \
+					--dbname=$MYSQL_DATABASE \
+					--dbuser=$MYSQL_USER \
+					--dbpass=$MYSQL_PASSWORD \
+					--dbhost=mariadb:3306
+
+	wp core install --allow-root \
+				--url=$DOMAIN_NAME \
+				--title=$TITLE \
+				--admin_user=$MYSQL_USER \
+				--admin_password=$MYSQL_PASSWORD \
+				--admin_email=$WP_ADMIN_EMAIL
+
+	wp user create --allow-root "${WP_USER}" "${WP_USER_EMAIL}" \
+				--user_pass=$WP_USER_PASSWORD \
+				--role=editor
+fi
+
+exec php-fpm8.2 -F
