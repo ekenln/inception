e51db83960820372026dbaad4faae70fed2d871c
ekenln <eklund.ellen.ee@gmail.com>
2026-03-20 17:38:14 +0100

    added if block to only do databse setup the first time

--- srcs/requirements/mariadb/tools/setup-mariadb.sh
+++ srcs/requirements/mariadb/tools/setup-mariadb.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-service mariadb start;
+if [ ! -d /var/lib/mysql/${DATABASE_NAME} ]; then
+	service mariadb start;
 
-until mysqladmin ping --silent; do
-    sleep 1
-done
+	until mysqladmin ping; do
+		sleep 1
+	done
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${DATABASE_NAME}\`;"
-mysql -u root -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON \`${DATABASE_NAME}\`.* TO '${MYSQL_USER}'@'%';"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+	mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${DATABASE_NAME}\`;"
+	mysql -u root -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON \`${DATABASE_NAME}\`.* TO '${MYSQL_USER}'@'%';"
+	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+	mysql -u root -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown;
+	mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown;
+fi
 
+echo "Starting mariaDB"
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/setup-wp.sh
+++ srcs/requirements/wordpress/tools/setup-wp.sh
@@ -29,11 +29,13 @@ if [ ! -f wp-config.php ]; then
 				--title=$TITLE \
 				--admin_user=$WP_ADMIN \
 				--admin_password=$WP_ADMIN_PASSWORD \
-				--admin_email=$WP_ADMIN_EMAIL
+				--admin_email=$WP_ADMIN_EMAIL \
+				--skip-email
 
 	wp user create --allow-root "${WP_USER}" "${WP_USER_EMAIL}" \
 				--user_pass=$WP_USER_PASSWORD \
 				--role=editor
 fi
 
+echo "starting WordPress"
 exec php-fpm8.2 -F
